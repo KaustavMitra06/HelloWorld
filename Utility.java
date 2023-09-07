585fcc346bc42568fedaa9c1b0722fa6148c6c23
kaust <24kmitra@overlake.org>
2023-09-06 20:43:19 -0700

    Add functionality that takes in a parameter

--- Utility.java
+++ Utility.java
@@ -1,5 +1,13 @@
 public class Utility {
-    public static void printHello() {
-        System.out.println("Hello World!");
+    static void printHelloWorld(String input){
+	if (input.equals("hello")){
+		System.out.println("Hello World!");
+	}
+	else if (input.equals("goodbye")){
+		System.out.println("Goodbye, cruel world!");
+	}
+	else {
+		System.out.println("Expected 'hello' or 'goodbye'");
+	}
     }
 }
